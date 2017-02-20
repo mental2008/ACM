17ef76ecaf5c40f37ae539ab9f9cbc9b764dd6f7
mental2008 <mental2008@163.com>
2017-02-20 16:39:41 +0800

    树形dp模板 同HDU_1520

---
+++ poj_2342.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#define maxn 6000
+using namespace std;
+int n;
+int dp[maxn+5][3];
+vector<int> vec[maxn+5];
+void dfs(int i, int fa){
+	for(int j = 0; j < vec[i].size(); j++){
+		if(vec[i][j] == fa) continue;
+		dfs(vec[i][j], i);
+		dp[i][0] += max(dp[vec[i][j]][1], dp[vec[i][j]][0]);
+		dp[i][1] += dp[vec[i][j]][0];
+	}
+}
+int main(){
+	while(~scanf("%d", &n)){
+		memset(dp, 0, sizeof(dp));
+		for(int i = 0; i <= maxn; i++) vec[i].clear();
+		for(int i = 1; i <= n; i++){
+			scanf("%d", &dp[i][1]);
+		}
+		int x, y;
+		while(scanf("%d%d", &x, &y)){
+			if(x == 0 && y == 0) break;
+			vec[x].push_back(y);
+			vec[y].push_back(x);
+		}
+		dfs(1, -1);
+		int ans = max(dp[1][0], dp[1][1]);
+		printf("%d\n", ans);
+	}
+	return 0;
+}
