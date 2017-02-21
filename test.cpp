058cc722b7d298aff04a6ec177fef467fc2c2079
mental2008 <mental2008@163.com>
2017-02-21 14:04:04 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,82 +1,117 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<string>
-#include<algorithm>
-#include<vector>
-#include<map>
-#define maxn 20000
+nclude <iostream>
+
+#include <stdio.h>
+
+#include <algorithm>
+
+
+
 using namespace std;
-vector<string> vec[maxn+5];
-map<string,int> lalala;
-int main()
-{
-	int n;
-	while(~scanf("%d",&n))
-	{
-		for(int i=0;i<=maxn;i++) vec[i].clear();
-		lalala.clear();
-		int k=0;
-		for(int i=0;i<n;i++)
-		{
-			string a;
-			cin>>a;
-			bool is_ok=false;
-			string tmp;
-			for(int j=0;j<a.size();j++)
-			{
-				if(is_ok==true)
-				{
-					tmp+=tolower(a[j]);
-				}
-				if(a[j]=='@') is_ok=true;
-			}
-			if(tmp=="bmail.com")
-			{
-				string copy;
-				for(int j=0;j<a.size();j++)
-				{
-					if(a[j]=='.') continue;
-					if(a[j]=='+'||a[j]=='@') break;
-					copy+=tolower(a[j]);
-				}
-				if(!lalala[copy])
-				{
-					vec[++k].push_back(a);
-					lalala[copy]=k;
-				}
-				else
-				{
-					vec[lalala[copy]].push_back(a);	
-				}
-			}
-			else
-			{
-				string copy;
-				for(int j=0;j<a.size();j++)
-					copy+=tolower(a[j]);	
-				if(!lalala[copy])
-				{
-					vec[++k].push_back(a);
-					lalala[copy]=k;
-				}
-				else
-				{
-					vec[lalala[copy]].push_back(a);
-				}
-			}
-		}
-		printf("%d\n",k);
-		for(int i=1;i<=k;i++)
-		{
-			printf("%d",(int)vec[i].size());
-			for(int j=0;j<vec[i].size();j++)
-			{
-				printf(" ");
-				cout<<vec[i][j];
-			}
-			printf("\n");
-		}
+
+
+
+long long a[1010], b[1010];
+
+double c[1010];
+
+int n, k;
+
+
+
+bool ok(double mid){
+
+	for (int i = 1; i <= n; i++){
+
+		c[i] = a[i] - b[i] * mid;
+
 	}
-	return 0;
+
+	sort(c + 1, c + 1 + n);
+
+	double sum = 0.0;
+
+	for (int i = k+1; i <= n; i++){
+
+		sum += c[i];
+
+	}
+
+	if (sum >= 0.0) return true;
+
+	return false;
+
 }
+
+
+
+int f(double ans){
+
+	int ans2 = ans;
+
+	if (ans - ans2 > ans2 + 1 - ans){
+
+		return ans2 + 1;
+
+	}
+
+	return ans2;
+
+}
+
+
+
+int main(){
+
+	while (scanf("%d%d", &n, &k) == 2){
+
+		if (n == 0 && k == 0) break;
+
+		
+
+		for (int i = 1; i <= n; i++){
+
+			scanf("%I64d", &a[i]);
+
+			a[i] = 100 * a[i];
+
+		}
+
+		for (int i = 1; i <= n; i++)
+
+			scanf("%I64d", &b[i]);
+
+
+
+		double l = 0.0, r = 100.0, mid, eps = 0.0001;
+
+
+
+		while (r - l > eps){
+
+			mid = (r + l) / 2;
+
+			if (ok(mid)) l = mid;
+
+			else r = mid;
+
+		}
+
+		printf("%d\n", f(mid));
+
+	}
+
+	return 0;
+
+}//test
+
+
+
+
+
+
+
+ 
+
+
+
+ 
